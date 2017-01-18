63b6003c34771e8c0ef4dc1c85630d3ef5769e93
yingyuelou@yeah.net <qazwsx1232>
2017-01-18 17:04:47 +0800

    完善内容

--- src/main/java/config/DataConfig.java
+++ src/main/java/config/DataConfig.java
@@ -1,4 +1,3 @@
-/*
 package config;
 
 import com.alibaba.druid.pool.DruidDataSource;
@@ -6,7 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 
 import javax.sql.DataSource;
 import java.util.Properties;
@@ -14,28 +15,22 @@ import java.util.Properties;
 @Configuration
 @ComponentScan
 @EnableTransactionManagement
-public class DataConfig {
+public class DataConfig implements TransactionManagementConfigurer {
 
   @Bean
-  public DataSource dataSource() {   */
-/*创建嵌入式数据库*//*
-
+  public DataSource dataSource() {   /*创建嵌入式数据库*/
     DruidDataSource ds=new DruidDataSource();
     ds.setUrl("127.0.0.1:3306");
     ds.setUsername("root");
     ds.setPassword("root");
     return ds;
   }
-  */
-/*声明Hibernate的Session工厂*//*
-
+  /*声明Hibernate的Session工厂*/
   @Bean
   public LocalSessionFactoryBean sessionFactory(DataSource dataSource){
     LocalSessionFactoryBean sfb=new LocalSessionFactoryBean();
     sfb.setDataSource(dataSource);
-    sfb.setPackagesToScan(new String[]{"domain"});*/
-/*packagesToScan属性告诉Spring扫描一个或多个包以查找域类，这些类通过注解的方式表明要使用Hibernate进行持久化，这些类可以使用的注解包括JPA的@Entity或@MappedSuperclass以及Hibernate的@Entity。*//*
-
+    sfb.setPackagesToScan(new String[]{"domain"});/*packagesToScan属性告诉Spring扫描一个或多个包以查找域类，这些类通过注解的方式表明要使用Hibernate进行持久化，这些类可以使用的注解包括JPA的@Entity或@MappedSuperclass以及Hibernate的@Entity。*/
 
 
     Properties props=new Properties();
@@ -45,4 +40,7 @@ public class DataConfig {
 
   }
 
-}*/
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        return null;
+    }
+}
