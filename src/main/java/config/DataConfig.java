23635b500feac59553c3a076ea0857df29c8bd41
yingyuelou@yeah.net <qazwsx1232>
2017-01-21 16:22:51 +0800

    数据库问题暂时删除

--- src/main/java/config/DataConfig.java
+++ src/main/java/config/DataConfig.java
@@ -1,53 +1,11 @@
 package config;
 
-import com.alibaba.druid.pool.DruidDataSource;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.Database;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan
-@EnableTransactionManagement
+
 public class DataConfig  {
-
-    @Bean
-    public DataSource dataSource() {
-        DruidDataSource dataSource = new DruidDataSource();
-
-        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/sushe?useUnicode=true&characterEncoding=UTF-8&useSSL=false");
-
-        dataSource.setUsername("root");
-        dataSource.setPassword("root");
-        return dataSource;
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
-        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
-        emf.setDataSource(dataSource);
-        emf.setPersistenceUnitName("spittr");
-        emf.setJpaVendorAdapter(jpaVendorAdapter);
-        emf.setPackagesToScan("web.domain");
-        return emf;
-    }
-
-    @Bean
-    public JpaVendorAdapter jpaVendorAdapter() {
-        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-        adapter.setDatabase(Database.MYSQL);
-        adapter.setShowSql(true);
-        adapter.setGenerateDdl(false);
-        adapter.setDatabasePlatform("org.hibernate.dialect.MYSQLDialect");
-        return adapter;
-    }
-
-
 
 }
