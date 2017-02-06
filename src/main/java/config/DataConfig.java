96199daea205478b3584916999adf012eb6c1f5a
yingyuelou@yeah.net <qazwsx1232>
2017-02-06 14:14:39 +0800

    数据库创建

--- src/main/java/config/DataConfig.java
+++ src/main/java/config/DataConfig.java
@@ -1,11 +1,58 @@
 package config;
 
-import org.springframework.context.annotation.ComponentScan;
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
 
 @Configuration
-@ComponentScan
-
+@EnableJpaRepositories(basePackages="web.Repository")
 public class DataConfig  {
+    @Bean
+    public DataSource dataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/sushe"  );
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        return dataSource;
+    }
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+        LocalContainerEntityManagerFactoryBean emfb=new LocalContainerEntityManagerFactoryBean();
+        emfb.setDataSource(dataSource());
+        emfb.setPackagesToScan("web.domain");
+        emfb.setJpaVendorAdapter(jpaVendorAdapter());
+        return emfb;
+    }
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter adapter=new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(false);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5InnoDBDialect");
+        return adapter;
+    }
 
+  /*  @Configuration
+    @EnableTransactionManagement
+    public static class TransactionConfig {
+
+        @Inject
+        private EntityManagerFactory emf;
+
+        @Bean
+        public PlatformTransactionManager transactionManager() {
+            JpaTransactionManager transactionManager = new JpaTransactionManager();
+            transactionManager.setEntityManagerFactory(emf);
+            return transactionManager;
+        }
+    }*/
 }
